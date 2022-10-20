447ed15b5a1142a10ce8aa22c219a9453d19deeb
DESKTOP-HK04HJD\Er <edwardyashchuk.w@gmail.com>
2022-10-20 23:41:12 +0300

    S1.2

--- Tasks/Tasks/Program.cs
+++ Tasks/Tasks/Program.cs
@@ -2,25 +2,25 @@
 //float number = 12.103F;
 //Console.WriteLine(number);
 
-Console.WriteLine("ЭЭЭЭЙ!!!Введите число, которое хотите укоротить до сотых:");
-string? userNumber = Console.ReadLine();
+//Console.WriteLine("ЭЭЭЭЙ!!!Введите число, которое хотите укоротить до сотых:");
+//string? userNumber = Console.ReadLine();
 
-if (string.IsNullOrEmpty(userNumber))
-{
-    Console.WriteLine("Ошибка, ты какашка! В следующий раз введи что-то ёмаё!");
-}
-else
-{
-    try
-    {
-        float trueNumber = Convert.ToSingle(userNumber);
-        Console.WriteLine($" Ну ладно, держи: {Math.Round(trueNumber, 3)}");
-    }
-    catch (FormatException)
-    {
-        Console.WriteLine("А по жопе? Что за фомат?");
-    }
-}
+//if (string.IsNullOrEmpty(userNumber))
+//{
+//    Console.WriteLine("Ошибка, ты какашка! В следующий раз введи что-то ёмаё!");
+//}
+//else
+//{
+//    try
+//    {
+//        float trueNumber = Convert.ToSingle(userNumber);
+//        Console.WriteLine($" Ну ладно, держи: {Math.Round(trueNumber, 3)}");
+//    }
+//    catch (FormatException)
+//    {
+//        Console.WriteLine("А по жопе? Что за фомат?");
+//    }
+//}
 
 
 
@@ -28,4 +28,23 @@ else
 
 //S.1.2 Вывести на экран число e (основание натурального логарифма) с точностью до десятых.
 
+Console.WriteLine("ЭЭЭЭЙ!!! Введи число:");
+string? numberE = Console.ReadLine();
+
+if (string.IsNullOrEmpty(numberE))
+{
+    Console.WriteLine("Ошибка, ты какашка! В следующий раз введи что-то ёмаё!");
+}
+else
+{
+    try
+    {
+        float trueNumber = Convert.ToSingle(numberE);
+        Console.WriteLine($" Ну ладно, держи: {Math.Log(trueNumber, 2)}");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("А по жопе? Что за фомат?");
+    }
+}
 
