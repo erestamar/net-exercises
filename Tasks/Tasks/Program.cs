b467e9cfbc193c4dd4b9bfc2b4aa94af1688e789
DESKTOP-HK04HJD\Er <edwardyashchuk.w@gmail.com>
2022-10-21 01:28:09 +0300

    S1.5.- S1.7.

--- Tasks/Tasks/Program.cs
+++ Tasks/Tasks/Program.cs
@@ -75,25 +75,51 @@
 */
 
 
-/*S.1.4
+/* S.1.4
     S.1.4 Составить программу вывода на экран числа, вводимого с клавиатуры. 
     После выводимого числа должно следовать сообщение » — вот какое число Вы  ввели».
+
+    Console.WriteLine("Ептыыдь!! Введи число любое:");
+    string? userText = Console.ReadLine();
+    if (string.IsNullOrEmpty(userText))
+    {
+        Console.WriteLine("Почему пустооооооо??!?!?!?!?!");
+    }
+    else
+    {
+        try
+        {
+            int userNumber = Convert.ToInt32(userText);
+            Console.WriteLine($"{userNumber} хехе, я так и думал то ты введёшь именно его");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ты чего?! Забыл как выглядят циферки, АААА?!?!?!");
+        }
+    }
 */
-Console.WriteLine("Ептыыдь!! Введи число любое:");
-string? userText = Console.ReadLine();
-if (string.IsNullOrEmpty(userText))
-{
-    Console.WriteLine("Почему пустооооооо??!?!?!?!?!");
-}
-else
-{
-    try
-    {
-        int userNumber = Convert.ToInt32(userText);
-        Console.WriteLine($"{userNumber} хехе, я так и думал то ты введёшь именно его");
-    }
-    catch (FormatException)
-    {
-        Console.WriteLine("Ты чего?! Забыл как выглядят циферки, АААА?!?!?!");
-    }
-}
+
+
+/* S1.5.- S1.7.
+    S1.5. Вывести на одной строке числа 1, 13 и 49 с одним пробелом между ними.
+
+    S1.6. Вывести на одной строке числа 7, 15 и 100 с двумя пробелами между ними.
+
+    S1.7. Составить программу вывода на экран в одну строку трех любых чисел с двумя
+    пробелами между ними.
+ */
+
+Console.WriteLine("Держи числа хахахаха: 1 13 49");
+int firstNumber = 7;
+int secondNumber = 15;
+int thirdNumber = 100;
+Console.WriteLine($"А вот ещё на! хахаха: {firstNumber}  {secondNumber}  {thirdNumber}");
+Console.WriteLine("А ну ка, запроси теперь у меня три любых числа, начни с первого:");
+string? userTextFirst = Console.ReadLine();
+Console.WriteLine("Теперь второе число:");
+string? userTextSecond = Console.ReadLine();
+Console.WriteLine("Теперь третье число:");
+string? userTextThird = Console.ReadLine();
+Console.WriteLine($"Хм... а вот и они:{userTextFirst}  {userTextSecond}  {userTextThird}");
+
+
